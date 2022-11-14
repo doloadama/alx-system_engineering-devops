860acb2350916877d7d4af23b23c70707e219214
doloadama <doloadama@outlook.com>
2022-11-14 19:02:01 +0000

    .

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-'''
-A script to export data in the JSON format
-'''
+"""extend your Python script to export data in the JSON format"""
 import json
 import requests
+from sys import argv
 
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/users"
-    us = requests.get(url, verify=False).json()
-    undoc = {}
-    udoc = {}
-    for user in us:
-        uid = user.get("id")
-        udoc[uid] = []
-        undoc[uid] = user.get("username")
-    url = "https://jsonplaceholder.typicode.com/todos"
-    todo = requests.get(url, verify=False).json()
-    [udoc.get(t.get("userId")).append({"task": t.get("title"),
-                                       "completed": t.get("completed"),
-                                       "username": undoc.get(
-                                               t.get("userId"))})
-     for t in todo]
-    with open("todo_all_employees.json", 'w') as jsf:
-        json.dump(udoc, jsf)
+
+if __name__ == "__main__":
+    """Your code should not be executed when imported"""
+
+    users = requests.get('{}/users'.format(
+        'https://jsonplaceholder.typicode.com')).json()
+    tasks = requests.get(
+        '{}/todos'.format('https://jsonplaceholder.typicode.com')).json()
+
+    out = dict()
+
+    for user in users:
+        out.update({user.get('id'): []})
+        for tarea in tasks:
+            if tarea.get('userId') == user.get('id'):
+                data = {
+                        'task': tarea.get('title'),
+                        'completed': tarea.get('completed'),
+                        'username': user.get('username')
+                }
+                out.get(user.get('id')).append(data)
+
+    with open('todo_all_employees.json', "w") as file:
+        json.dump(out, file)
