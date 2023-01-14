1d550f3b1472ad150ed0cfee9186d0f8f6572d26
doloadama <doloadama@outlook.com>
2023-01-14 00:18:45 +0000

    .

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-# Sky is the limit, let's bring that limit higher
-exec { 'Skylimit':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx; service nginx restart',
-  provider => shell
+# Fixing the number of failed requests to get to 0
+exec { 'fix--for-nginx':
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
